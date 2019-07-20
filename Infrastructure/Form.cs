c3a6bfc0404cf598345b5c097ae0c15f3142758d
yxbysxcoco <liangzhipipi@163.com>
2019-07-20 11:56:34 +0800

    修改查询框组件

--- Infrastructure/Form.cs
+++ Infrastructure/Form.cs
@@ -8,9 +8,9 @@ namespace MES.Infrastructure
 {
     public static class Form
     {
-        public static MvcHtmlString Input(this HtmlHelper html, String id, String type)
+        public static MvcHtmlString Input(this HtmlHelper html, String id, String type, String placeholder)
         {
-            string input = String.Format("<input class='form-control' type='{0}' id='{1}' />", type, id);
+            string input = String.Format("<input class='form-control' type='{0}' id='{1}' placeholder='{2}' />", type, id, placeholder);
             return new MvcHtmlString(input);
         }
         public static MvcHtmlString Button(this HtmlHelper html, string id)
@@ -20,7 +20,7 @@ namespace MES.Infrastructure
         }
         public static MvcHtmlString DatePicker(this HtmlHelper html, string id)
         {
-            string datepicker = String.Format("<input class='form-control' id='{0}' />", id);
+            string datepicker = String.Format("<input name='datePicker' class='form-control' id='{0}' />", id);
             return new MvcHtmlString(datepicker);
         }
     }
