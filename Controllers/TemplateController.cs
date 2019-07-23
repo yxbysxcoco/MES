1bab5087dd4e79f4b7af7ddd4035ec12253ada61
yxbysxcoco <liangzhipipi@163.com>
2019-07-23 16:44:28 +0800

    cnm

---
+++ Controllers/TemplateController.cs
@@ -0,0 +1,26 @@
+﻿using SQ_DB_Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MES.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult Button()
+        {
+            return View();
+        }
+        public ActionResult Input()
+        {
+            return View();
+        }
+        public ActionResult Tem()
+        {
+            return View(new ToolEquipment());
+        }
+    }
+}
--- Controllers/ToolController.cs
+++ Controllers/ToolController.cs
@@ -17,26 +17,6 @@ namespace MES.Controllers
         {
             get { return new JsonTool("menu.json").GetValueList<Sider>("Menu").Where(m => m.ControllerName == "Tool").ToList(); }
         }
-        public ActionResult Index()
-        {
-            return View(menu);
-        }
-        public ActionResult Template()
-        {
-            return View(menu);
-        }
-        public ActionResult Excel()
-        {
-            return View("",  menu);
-        }
-        public ActionResult SendMsg()
-        {
-            return View(menu);
-        }
-        public ActionResult ReceiveMsg()
-        {
-            return View(menu);
-        }
         public ActionResult Tem()
         {
             return View(new ToolEquipment());
