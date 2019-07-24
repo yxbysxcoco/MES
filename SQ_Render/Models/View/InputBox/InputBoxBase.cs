b2d64f7142568eb129807c8e7540e9f42be88ac4
少波 邢 <1053302852@qq.com>
2019-07-24 11:42:29 +0800

    add inputBox class

---
+++ SQ_Render/Models/View/InputBox/InputBoxBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SQ_Render.Models.View
+{
+    public class InputBoxBase : Element
+    {
+        public string Alias { get; set; }
+        public bool IsRequired { get; set; }
+        private InputBoxBase() { }
+        public InputBoxBase(string alias, bool isRequired)
+        {
+            Alias = alias;
+            IsRequired = isRequired;
+        }
+    }
+}
---
+++ SQ_Render/Models/View/InputBox/PasswordInputBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQ_Render.Models.View.InputBox
+{
+    public class PasswordInputBox : InputBoxBase
+    {
+        public PasswordInputBox(string aliaes, bool isRqeuired):base(aliaes, isRqeuired) {}
+        public override MvcHtmlString Render()
+        {
+            return base.Render();
+        }
+    }
+}
