0a05391dc96cc9e0f8bd0026467ec93c75855f92
yxbysxcoco <liangzhipipi@163.com>
2019-07-24 20:07:49 +0800

    111

---
+++ SQ_Render/Models/View/Collection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQ_Render.Models.View
+{
+    public class Collection : AbstractElement
+    {
+        public Dictionary<String, String> Lis { get; set; }
+        public override MvcHtmlString Render()
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            ul.AddCssClass("collection");
+
+            foreach(String key in Lis.Keys)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.AddCssClass("collection-item");
+                li.MergeAttribute("href", Lis[key]);
+
+                ul.InnerHtml += li.ToString();
+            }
+            return new MvcHtmlString(ul.ToString());
+        }
+    }
+}
