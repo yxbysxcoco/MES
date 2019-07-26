051ff703d60b9826d36d8826fc32efbb11259562
yxbysxcoco <liangzhipipi@163.com>
2019-07-26 19:02:39 +0800

    1111

---
+++ SQ_Render/Models/View/Containers/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQ_Render.Models.View.Containers
+{
+    public class Context : Container
+    {
+        public override string TagName => "div";
+        public override void InitTag(HtmlHelper htmlHelper, TagBuilder tag)
+        {
+            base.InitTag(htmlHelper, tag);
+            tag.AddCssClass("card-content");
+        }
+    }
+}
