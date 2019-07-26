dd47e20e77f9bad395e8eb30ca6554fcaa2a4c7b
少波 邢 <1053302852@qq.com>
2019-07-26 11:35:39 +0800

    修改侧边栏

--- SQ_Render/Models/View/Sider.cs
+++ SQ_Render/Models/View/Sider.cs
@@ -9,17 +9,21 @@ namespace SQ_Render.Models.View.Components
 {
     public class Sider : AbstractElement
     {
-        public Dictionary<string, string> Lis { get; set; }
+        public Dictionary<string, Tuple<string, string>> Lis { get; set; }
         public IFrame iframe { get; set; }
         public override string TagName => "ul";
         public override void InitTag(HtmlHelper htmlHelper, TagBuilder tag)
         {
+            UrlHelper url = new UrlHelper();
+
             base.InitTag(htmlHelper, tag);
-            foreach(var key in Lis.Keys)
+            foreach(var kv  in Lis)
             {
+                string link = url.Action(kv.Value.Item1, kv.Value.Item2);
+
                 TagBuilder li = new TagBuilder("li");
                 TagBuilder a = new TagBuilder("a");
-                a.InnerHtml = Lis[key];
+                a.InnerHtml = kv.Key;
                 a.AddCssClass("class='waves-effect'");
                 li.InnerHtml = a.ToString();
                 tag.InnerHtml += li;
