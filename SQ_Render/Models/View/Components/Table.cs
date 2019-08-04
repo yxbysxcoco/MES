bd72ea7466e0d27611aeeccaf849ae9e265a5543
yxbysxcoco <liangzhipipi@163.com>
2019-08-04 10:44:59 +0800

    更新表格

--- SQ_Render/Models/View/Components/Table.cs
+++ SQ_Render/Models/View/Components/Table.cs
@@ -20,57 +20,11 @@ namespace SQ_Render.Models.View.Components
         public override string TagName => "table";
         public override void InitTag(HtmlHelper htmlHelper, TagBuilder tag)
         {
-            AddChildElement(new IFrame($""));
+            AddChildElement(new IFrame($"initTable('{Id}', '{DataTable.ToJSON()}')"));
             base.InitTag(htmlHelper, tag);
-            //tag.MergeAttribute("id", Id);
-            //tag.AddCssClass("highlight");
-
-            //TagBuilder thead = new TagBuilder("thead");
-            //TagBuilder tr_head = new TagBuilder("tr");
-            //foreach (var head in DataTable.Columns)
-            //{
-            //    TagBuilder th = new TagBuilder("th");
-            //    th.InnerHtml = head.Alais;
-            //    th.MergeAttribute("name", head.Name);
-            //    tr_head.InnerHtml += th.ToString();
-            //}
-            //thead.InnerHtml = tr_head.ToString();
-            //TagBuilder tbody = new TagBuilder("tbody");
-            //tbody.MergeAttribute("id", "tbody");
-            //foreach (var row in DataTable)
-            //{
-            //    TagBuilder tr = new TagBuilder("tr");
-            //    tr.MergeAttribute("id", Current.ToString());
-
-            //    tr.MergeAttribute("hidden", "");
-            //    Current = Current + 1;
-            //    foreach(var body in row)
-            //    {
-            //        TagBuilder td = new TagBuilder("td");
-            //        td.InnerHtml = body.ToString();
-            //        tr.InnerHtml += td.ToString();
-            //    }
-            //    tbody.InnerHtml += tr;
-            //}
-            //AddChildElement(new PageNav());
-            //tag.InnerHtml = thead.ToString();
-            //tag.InnerHtml += tbody.ToString();
-        }
-    }
-    public class PageNav: AbstractElement
-    {
-        public override string TagName => "ul";
-        public override void InitTag(HtmlHelper htmlHelper, TagBuilder tag)
-        {
-            base.InitTag(htmlHelper, tag);
-            tag.AddCssClass("pagination");
-            tag.MergeAttribute("id", "page");
-            TagBuilder toLeft = new TagBuilder("li");
-            toLeft.InnerHtml = @"<a href='#!'><i class='material-icons'>chevron_left</i></a>";
-            TagBuilder toRight = new TagBuilder("li");
-            toRight.InnerHtml = @"<a href='#!'><i class='material-icons'>chevron_right</i></a>";
-            tag.InnerHtml += toLeft;
-            tag.InnerHtml += toRight;
+            tag.MergeAttribute("id", Id);
+            tag.MergeAttribute("lay-filter", "table");
+            tag.AddCssClass("layui-hide");
         }
     }
 }
