cab0ccfc1b4dd40aa68ce96cbcb481aee7eac662
yxbysxcoco <liangzhipipi@163.com>
2019-08-04 20:40:19 +0800

    新增多级表头

--- SQ_Render/Controllers/HomeController.cs
+++ SQ_Render/Controllers/HomeController.cs
@@ -79,15 +79,25 @@ namespace SQ_Render.Controllers
 
         public ActionResult Table()
         {
-            var dbSet = new SQDbSet<ToolEquipment>();
-            var entities = dbSet.GetAllEntities();
+            Dictionary<string, string> entityInfoDic = new Dictionary<string, string>();
+            var sQDbSet = new SQDbSet<ToolEquipment>();
 
-            var dataTable = new DataTable();
+            //var entities = sQDbSet.GetEntitiesByContion(entityInfoDic);
+            var pageHelper = sQDbSet.GetEntitiesByCondition(1, 10, entityInfoDic, "");
 
-            dataTable.BuildReduceDataTable(entities, t => new { t.Code, t.RepairCycle },
-                   l => l.Sum(t => t.Weight),
-                   l => l.Average(t => t.Univalence),
-                   l => l.Max(t => t.Edition));
+            DataTable dataTable = new DataTable();
+
+            //dataTable.BuildRepalceDataTable(pageHelper.AllList, t =>t.Name ,t => t.Weight, t => DataTable.Repalce(t.TypeId,t.ToolEquipmentType.Name),t=>DataTable.Repalce(t.MoneyUnitId,t.MoneyUnit.Name));
+
+            dataTable.SetColumn<ToolEquipment>(t => DataTable.Multistage(t.Code, 2), t => DataTable.Multistage(t.Name, 2, "1"));
+            dataTable.SetColumn<ToolEquipment>(t => t.Weight, t => t.Mark);
+            dataTable.SetRow(pageHelper.AllList, t => t.Code, t => t.Weight, t => t.Mark);
+
+            dataTable.PageIndex = 1;
+            dataTable.PageSize = 10;
+            dataTable.TotalCount = pageHelper.TotalCount;
+            dataTable.Limits = new int[3] { 10, 15, 20 };
+            dataTable.TableName = "工装表";
             var table = new Table("t1", dataTable);
 
             return View(table);
